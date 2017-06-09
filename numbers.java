f9b3deb985790bd35e59a06169d515469b53b305
Aswinig18 <gaswini313@gmail.com>
2017-06-09 17:48:01 +0530

    Create numbers.java

---
+++ numbers.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class numbers
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int t=s.nextInt();
+if(t%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
